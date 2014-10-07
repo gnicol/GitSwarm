345fe4dd95f0d9f43aad137836dcd64294e29a77
Rusty Phillips <rusty@cloudspace.com>
2014-10-07 16:46:04 -0400

    Removed tests that require delays. 	deleted:    perforce_swarm/db/migrate/20141001001018_add_source_control_type.rb 	deleted:    perforce_swarm/spec/models/project_spec.rb

--- perforce_swarm/db/migrate/20141001001018_add_source_control_type.rb
+++
@@ -1,7 +0,0 @@
-class AddSourceControlType < ActiveRecord::Migration
-  def change
-	change_table :projects do |t|
-		t.string :source_control_type, :default=>"perforce"
-	end
-  end
-end
--- perforce_swarm/spec/models/project_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe Project do
-    let!(:project) { create(:project) }
-    describe "makes sure that a migration was added from the rails instance." do
-   	it { should respond_to(:source_control_type) }
-    end
-end

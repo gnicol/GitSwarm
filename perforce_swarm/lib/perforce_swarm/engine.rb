28f27d3f0250b412721eb878e63748f56ebaaae8
Rusty Philips <rusty@cloudspace.com>
2014-10-07 12:33:30 -0700

    Fixed indentation.

--- perforce_swarm/lib/perforce_swarm/engine.rb
+++ perforce_swarm/lib/perforce_swarm/engine.rb
@@ -1,7 +1,7 @@
 module PerforceSwarm
   class Engine < ::Rails::Engine
 	
- initializer :append_migrations do |app|
+  initializer :append_migrations do |app|
       unless app.root.to_s.match root.to_s
         config.paths["db/migrate"].expanded.each do |expanded_path|
           app.config.paths["db/migrate"] << expanded_path

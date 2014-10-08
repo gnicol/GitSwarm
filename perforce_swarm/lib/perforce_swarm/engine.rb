303a774f9c7e8476f49a53f3487d6de5b51c9036
Rusty Phillips <rusty@cloudspace.com>
2014-10-08 17:19:31 -0400

    Changed engine. 	modified:   engine.rb

--- perforce_swarm/lib/perforce_swarm/engine.rb
+++ perforce_swarm/lib/perforce_swarm/engine.rb
@@ -1,6 +1,4 @@
 module PerforceSwarm
-  class Engine < ::Rails::Engine
-	
   # Change the railties order so our engine comes first
   # This allows our routes and asset_paths to take precedence
   module ConfigurationExtension
@@ -10,9 +8,10 @@ module PerforceSwarm
     end
   end
  
+  class Engine < ::Rails::Engine
   # We want our engine's migrations to be run when the main app runs db:migrate
   # It seems as though stand-alone initializers run too late so the logic is here.
-  initializer :append_migrations do |app|
+    initializer :append_migrations do |app|
       unless app.root.to_s.match root.to_s
         config.paths["db/migrate"].expanded.each do |expanded_path|
           app.config.paths["db/migrate"] << expanded_path

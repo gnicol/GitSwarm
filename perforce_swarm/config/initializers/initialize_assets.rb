77d3524621dc82fad7c00173ce4188137ee12b5d
Rusty Phillips <rusty@cloudspace.com>
2014-10-08 16:56:23 -0400

    Removing indentation.

--- perforce_swarm/config/initializers/initialize_assets.rb
+++ perforce_swarm/config/initializers/initialize_assets.rb
@@ -1,6 +1,6 @@
-    assets = Rails.root.join("perforce_swarm/app/assets/")
-    Rails.configuration.assets.paths = Dir.entries(assets)
-    .select { |f| f!="." && f!=".." && File.directory?(assets.join(f)) }
-    .map { |f| assets.join(f).to_s }
-    .concat(Rails.configuration.assets.paths)
+assets = Rails.root.join("perforce_swarm/app/assets/")
+Rails.configuration.assets.paths = Dir.entries(assets)
+   .select { |f| f!="." && f!=".." && File.directory?(assets.join(f)) }
+   .map { |f| assets.join(f).to_s }
+   .concat(Rails.configuration.assets.paths)
 

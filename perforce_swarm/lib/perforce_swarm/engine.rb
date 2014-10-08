97200dc96abd551fa33d4553838640f5c3bd9691
Rusty Philips <rusty@cloudspace.com>
2014-10-08 11:14:29 -0700

    Added a comment for why the initializer is here.

--- perforce_swarm/lib/perforce_swarm/engine.rb
+++ perforce_swarm/lib/perforce_swarm/engine.rb
@@ -10,6 +10,8 @@ module PerforceSwarm
     end
   end
   
+  # We want our engine's migrations to be run when the main app runs db:migrate
+  # It seems as though stand-alone initializers run too late so the logic is here.
   initializer :append_migrations do |app|
       unless app.root.to_s.match root.to_s
         config.paths["db/migrate"].expanded.each do |expanded_path|

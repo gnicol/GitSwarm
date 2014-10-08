ded2c9d712ef38e436c13197422ff5fbd0ead1ce
Rusty Phillips <rusty@cloudspace.com>
2014-10-08 16:42:20 -0400

    Fixed indentation.
    	modified:   spec/spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -43,21 +43,21 @@ RSpec.configure do |config|
   end
 
   def testdescription(metadata)
-      if metadata[:example_group].nil? || metadata[:example_group][:description_args].length==0
+     if metadata[:example_group].nil? || metadata[:example_group][:description_args].length==0
         return metadata[:description_args][0].to_s
-        else
-          return testdescription(metadata[:example_group])+"/"+metadata[:description_args][0].to_s
+     else
+        return testdescription(metadata[:example_group])+"/"+metadata[:description_args][0].to_s
         end
-      end
+     end
 
-    config.around(:each) do |test|
-        path = test.metadata[:file_path].gsub(/^.*\/spec/, "./spec") +','+testdescription(test.metadata)
-        if(!$rspec_pathlist.include?(path) ||
-          $rspec_pathlist[path]==test.metadata[:file_path])
-          #Unfortunately, multiple tests will result in the same path if the test is not described with an "it" block (this is pretty simple).  
-          #We therefore only allow dupes in the same test file.
-            $rspec_pathlist[path] = test.metadata[:file_path]
-            test.run
-        end
+   config.around(:each) do |test|
+     path = test.metadata[:file_path].gsub(/^.*\/spec/, "./spec") +','+testdescription(test.metadata)
+     if(!$rspec_pathlist.include?(path) || $rspec_pathlist[path]==test.metadata[:file_path])
+       #Unfortunately, multiple tests will result in the same path if the test is not described with 
+       # an "it" block (this is pretty simple).  
+       #We therefore only allow dupes in the same test file.
+          $rspec_pathlist[path] = test.metadata[:file_path]
+          test.run
+       end
      end
 end

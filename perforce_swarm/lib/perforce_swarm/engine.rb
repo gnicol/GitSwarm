103ca885afddaf9cdbc829072c3f53c01e745713
Rusty Philips <rusty@cloudspace.com>
2014-10-08 09:42:41 -0700

    Changed to address comment.

--- perforce_swarm/lib/perforce_swarm/engine.rb
+++ perforce_swarm/lib/perforce_swarm/engine.rb
@@ -1,21 +1,21 @@
 module PerforceSwarm
   class Engine < ::Rails::Engine
 	
-  initializer :append_migrations do |app|
-      unless app.root.to_s.match root.to_s
-        config.paths["db/migrate"].expanded.each do |expanded_path|
-          app.config.paths["db/migrate"] << expanded_path
-        end
-      end
-    end
-  end
-
   # Change the railties order so our engine comes first
   # This allows our routes and asset_paths to take precedence
   module ConfigurationExtension
     def initialize(*)
       super
       @railties_order = [PerforceSwarm::Engine, :main_app, :all]
+    end
+  end
+  
+  initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
+      end
     end
   end
 end

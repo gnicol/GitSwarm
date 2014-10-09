bd698dee51624fda2874e41141c3915d5a432839
GitLab <gitlab@localhost>
2014-10-09 14:44:57 -0700

    Moved config.yml to config.yml.example. Renamed 'perforce' section to 'p4'.

--- perforce_swarm/config/initializers/config.rb
+++ perforce_swarm/config/initializers/config.rb
@@ -1,9 +1,13 @@
 module PerforceSwarm
   class Config < Settingslogic
-    source "#{__dir__}/../config.yml"
+    if File.exists? "#{__dir__}/../config.yml"
+      source("#{__dir__}/../config.yml")
+    else
+      source({})
+    end
     namespace Rails.env
   end
 end
 
 # Perforce Config
-PerforceSwarm::Config['perforce'] ||= Settingslogic.new({})
+PerforceSwarm::Config['p4'] ||= Settingslogic.new({})

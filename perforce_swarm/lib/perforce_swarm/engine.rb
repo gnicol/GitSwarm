8aa7186b34135e9d978d9ac5cff7cd5298376518
Geoff Nicol <gnicol@perforce.com>
2014-10-09 11:49:21 -0700

    Tweaking style for comment and module/class ordering. The initializer comment is now indented a tab deeper to line up with the code its on. The class definition now comes before the module ConfigurationExtension as that seems more to least important.

--- perforce_swarm/lib/perforce_swarm/engine.rb
+++ perforce_swarm/lib/perforce_swarm/engine.rb
@@ -1,22 +1,22 @@
 module PerforceSwarm
-  # Change the railties order so our engine comes first
-  # This allows our routes and asset_paths to take precedence
-  module ConfigurationExtension
-    def initialize(*)
-      super
-      @railties_order = [PerforceSwarm::Engine, :main_app, :all]
-    end
-  end
- 
   class Engine < ::Rails::Engine
-  # We want our engine's migrations to be run when the main app runs db:migrate
-  # It seems as though stand-alone initializers run too late so the logic is here.
+    # We want our engine's migrations to be run when the main app runs db:migrate
+    # It seems as though stand-alone initializers run too late so the logic is here
     initializer :append_migrations do |app|
       unless app.root.to_s.match root.to_s
         config.paths["db/migrate"].expanded.each do |expanded_path|
           app.config.paths["db/migrate"] << expanded_path
         end
       end
+    end
+  end
+  
+  # Change the railties order so our engine comes first
+  # This allows our routes and asset_paths to take precedence
+  module ConfigurationExtension
+    def initialize(*)
+      super
+      @railties_order = [PerforceSwarm::Engine, :main_app, :all]
     end
   end
 end

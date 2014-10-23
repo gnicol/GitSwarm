1c5b879e7be91af0afe7e05e08d4b143ef58cd31
Rusty Phillips <rusty@cloudspace.com>
2014-10-23 12:09:56 -0400

    Reverted spec_helper. 	modified:   spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -41,25 +41,4 @@ RSpec.configure do |config|
   config.before(:suite) do
     TestEnv.init
   end
-
-  def testdescription(metadata)
-    if metadata[:example_group].nil? || metadata[:example_group][:description_args].length==0
-      return metadata[:description_args][0].to_s
-    else
-      return testdescription(metadata[:example_group])+"/"+metadata[:description_args][0].to_s
-    end
-  end
-  
-  $rspec_pathlist = Hash.new
-   
-  config.around(:each) do |test|
-    path = test.metadata[:file_path].gsub(/^.*\/spec/, "./spec") +','+testdescription(test.metadata)
-    if(!$rspec_pathlist.include?(path) || $rspec_pathlist[path]==test.metadata[:file_path])
-      # Unfortunately, multiple tests will result in the same path if the test is not described with 
-      # an "it" block (this is pretty simple).  
-      # We therefore only allow dupes in the same test file.
-      $rspec_pathlist[path] = test.metadata[:file_path]
-      test.run
-    end
-  end
 end

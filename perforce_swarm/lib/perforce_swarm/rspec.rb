f5225141f025f318940d4aa251246a0e67240d29
Michael Orr <michael@cloudspace.com>
2014-10-31 18:09:45 +0000

    updates for rubocop, one offense about the lambda style

--- perforce_swarm/lib/perforce_swarm/rspec.rb
+++ perforce_swarm/lib/perforce_swarm/rspec.rb
@@ -46,7 +46,7 @@ RSpec.configure do |config|
     puts "\n#{skip_count} example#{'s' if skip_count > 1} skipped" if skip_count > 0
   end
 
-  config.filter_run_excluding :example_group => lambda { |example_group_meta, metadata|
+  config.filter_run_excluding example_group: lambda { |_example_group_meta, metadata|
     return false if metadata.key?(:override) && metadata[:override] == true
     if in_file?(override_file, override_label(metadata))
       skip_count += 1

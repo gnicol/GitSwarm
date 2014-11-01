0b33c64ef41a48ca2ac9827d5794814bc4e603ef
Michael Orr <michael@cloudspace.com>
2014-11-01 07:14:02 +0000

    moving the tag to a describe block

--- perforce_swarm/spec/lib/gitlab/ldap/adapter_spec.rb
+++ perforce_swarm/spec/lib/gitlab/ldap/adapter_spec.rb
@@ -3,26 +3,26 @@ require 'spec_helper'
 describe Gitlab::LDAP::Adapter do
   let(:adapter) { Gitlab::LDAP::Adapter.new 'ldap' }
 
-  describe :dn_matches_filter? do
+  describe :dn_matches_filter?, override: true do
     let(:ldap) { double(:ldap) }
     subject { adapter.dn_matches_filter?(:dn, :filter) }
     before { adapter.stub(ldap: ldap) }
 
     context 'when the search is successful' do
-      context 'and the result is non-empty', override: true do
+      context 'and the result is non-empty' do
         before { ldap.stub(search: [:foo]) }
 
         it { should be_true }
       end
 
-      context 'and the result is empty', override: true do
+      context 'and the result is empty' do
         before { ldap.stub(search: []) }
 
         it { should be_false }
       end
     end
 
-    context 'when the search encounters an error', override: true do
+    context 'when the search encounters an error' do
       before { ldap.stub(search: nil, get_operation_result: double(code: 1, message: 'some error')) }
 
       it { should be_false }

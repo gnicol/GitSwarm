d9336f046467ec1eb0b631972a7878b771bc04f1
Geoff Nicol <gnicol@perforce.com>
2014-11-12 14:16:05 -0800

    Adding some whitespace to improve readability

--- perforce_swarm/features/steps/dashboard/event_filters.rb
+++ perforce_swarm/features/steps/dashboard/event_filters.rb
@@ -3,16 +3,20 @@ require Rails.root.join('features', 'steps', 'dashboard', 'event_filters')
 class Spinach::Features::EventFilters < Spinach::FeatureSteps
   step 'this merge request has a comment' do
     visit merge_requests_dashboard_path
+    
     # We are at the merge request dashboard page and must click on the everyone's filter to see the merge request
     within '.scope-filter' do
       click_link 'Everyone\'s'
     end
+
     merge_request = MergeRequest.last
     click_link merge_request.title
+    
     # Fill in the comment form
     within('.js-main-target-form') do
       fill_in 'note[note]', with: 'Excellent merge'
     end
+    
     # Submit the comment
     within('.js-main-target-form') do
       click_button 'Add Comment'

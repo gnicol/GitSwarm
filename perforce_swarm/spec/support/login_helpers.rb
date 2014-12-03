48d19326e7470362d979c3b58bd532a3c08ebf20
DJ Mountney <dmountney@perforce.com>
2014-12-03 15:37:48 -0800

    Make rubocop happy

--- perforce_swarm/spec/support/login_helpers.rb
+++ perforce_swarm/spec/support/login_helpers.rb
@@ -12,9 +12,9 @@ module LoginHelpers
   # user - User instance to login with
   def login_with(user)
     visit new_user_session_path
-    fill_in "user_login", with: user.email
-    fill_in "user_password", with: "12345678"
-    click_button "Log in"
+    fill_in 'user_login', with: user.email
+    fill_in 'user_password', with: '12345678'
+    click_button 'Log in'
     Thread.current[:current_user] = user
   end
 end

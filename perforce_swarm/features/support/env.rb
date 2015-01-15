0dd915ea4e42c5423c2d1c98d6742505f8125a2b
tkim <tkim@perforce.com>
2015-01-14 17:34:50 -0800

    Needed to create wait_for_ajax call.

--- perforce_swarm/features/support/env.rb
+++ perforce_swarm/features/support/env.rb
@@ -66,3 +66,12 @@ Spinach.hooks.before_run do
 
   include FactoryGirl::Syntax::Methods
 end
+
+def wait_for_ajax
+  Timeout.timeout(Capybara.default_wait_time) do
+    loop do
+      active = page.evaluate_script('jQuery.active')
+      break if active == 0
+    end
+  end
+end

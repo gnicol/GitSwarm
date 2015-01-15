d8c0347bef6bc8a3c28bc90cf6ebe412cc5b048f
tkim <tkim@perforce.com>
2015-01-14 17:34:17 -0800

    Adding automated tests for cases when invalid user or characters are used to add users to projects.

---
+++ perforce_swarm/features/steps/project/team_management.rb
@@ -0,0 +1,33 @@
+require Rails.root.join('features', 'steps', 'project', 'team_management')
+class Spinach::Features::ProjectTeamManagement < Spinach::FeatureSteps
+  step 'I click link \'New Team Member\'' do
+    click_link 'New project member'
+  end
+
+  step 'I attempt to add a non-existent user in the People field' do
+    fill_in('s2id_autogen1', with: 'nonexistent-user')
+    wait_for_ajax
+  end
+
+  step 'I should see \'No matches found\'' do
+    all('ul.select2-results')[1].text.should have_content('No matches found')
+  end
+
+  step 'I attempt to add \'*$%&\' in the People field' do
+    fill_in('s2id_autogen1', with: '*$%&')
+    wait_for_ajax
+  end
+
+  step 'I attempt to add \'@^!()\' in the People field' do
+    fill_in('s2id_autogen1', with: '@^!()')
+    wait_for_ajax
+  end
+
+  step 'I click on the \'Add users\' button' do
+    find(:css, 'input.btn.btn-create').trigger('click')
+  end
+
+  step 'I should still be on the \'New project member(s)\' page' do
+    find(:css, '.page-title').should have_content('New project member(s)')
+  end
+end

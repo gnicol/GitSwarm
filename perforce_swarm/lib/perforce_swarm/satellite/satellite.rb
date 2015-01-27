b42a36cadea9ba58c456351ce4457bbfb1967113
DJ Mountney <dmountney@perforce.com>
2015-01-27 10:59:36 -0800

    Move our shell require so that it doesn't get pulled in during initilization. (Which would be before one could even run the shell install rake task)

--- perforce_swarm/lib/perforce_swarm/satellite/satellite.rb
+++ perforce_swarm/lib/perforce_swarm/satellite/satellite.rb
@@ -1,10 +1,10 @@
 require Rails.root.join('lib', 'gitlab', 'satellite', 'satellite')
-require File.join(Gitlab.config.gitlab_shell.path, 'perforce_swarm', 'mirror')
 
 module PerforceSwarm
   module GitlabSatelliteExtension
     def update_from_source!
       # if we have a 'mirror' remote, pull from it before proceeding
+      require File.join(Gitlab.config.gitlab_shell.path, 'perforce_swarm', 'mirror')
       PerforceSwarm::Mirror.fetch(project.repository.path_to_repo)
 
       super

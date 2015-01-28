92dfcd76ac17ca55769483c2a52bca508607ce1d
DJ Mountney <dmountney@perforce.com>
2015-01-28 15:50:26 -0800

    Turn on debugging

--- perforce_swarm/spec/spec_helper.rb
+++ perforce_swarm/spec/spec_helper.rb
@@ -1,1 +1,11 @@
-require File.expand_path('../../spec/spec_helper', __FILE__)
+require File.expand_path('../../../spec/spec_helper', __FILE__)
+
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(
+    app,
+    js_errors: false,
+    debug: true,
+    timeout: 90,
+    phantomjs_options: ['--proxy-type=socks5', '--proxy=0.0.0.0:0', '--debug=true']
+  )
+end

2b7937ca41c0da2fdfe2f1a8f5e02d40be66b452
DJ Mountney <dmountney@perforce.com>
2015-01-29 14:14:49 -0800

    Updated so that if we only specify the repo, it the default shell version will be used

--- perforce_swarm/spec/support/test_env.rb
+++ perforce_swarm/spec/support/test_env.rb
@@ -2,11 +2,10 @@ require Rails.root.join('spec', 'support', 'test_env')
 
 module TestEnv
   def setup_gitlab_shell
-    ref  = ENV['GITLAB_SHELL_REF']  || nil
-    repo = ENV['GITLAB_SHELL_REPO'] || nil
-
-    if ref && repo
-      `rake 'gitlab:shell:install[#{ref},#{repo}]'`
+    if ENV['GITLAB_SHELL_REPO']
+      default_version = File.read(Rails.root.join('GITLAB_SHELL_VERSION')).strip
+      ref  = ENV['GITLAB_SHELL_REF']  || "v#{default_version}"
+      `rake 'gitlab:shell:install[#{ref},#{ENV['GITLAB_SHELL_REPO']}]'`
     else
       `rake gitlab:shell:install`
     end

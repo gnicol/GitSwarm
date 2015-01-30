d3d90f0e2fe6f8c9631371005283ddd17cb2b7eb
DJ Mountney <dmountney@perforce.com>
2015-01-30 11:44:15 -0800

    Switch our rails_env to using prepend

--- perforce_swarm/spec/support/test_env.rb
+++ perforce_swarm/spec/support/test_env.rb
@@ -1,13 +1,17 @@
 require_relative '../../../spec/support/test_env'
 
-module TestEnv
-  def setup_gitlab_shell
-    if ENV['GITLAB_SHELL_REPO']
+module PerforceSwarm
+  module TestEnv
+    def setup_gitlab_shell
+      return super unless ENV['GITLAB_SHELL_REPO']
+
       default_version = File.read(Rails.root.join('GITLAB_SHELL_VERSION')).strip
       ref  = ENV['GITLAB_SHELL_REF']  || "v#{default_version}"
       `rake 'gitlab:shell:install[#{ref},#{ENV['GITLAB_SHELL_REPO']}]'`
-    else
-      `rake gitlab:shell:install`
     end
   end
+end
+
+module TestEnv
+  prepend PerforceSwarm::TestEnv
 end

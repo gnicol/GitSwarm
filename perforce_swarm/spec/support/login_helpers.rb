c91f08d270ac05bc556b513b70f406d44e848c77
DJ Mountney <dmountney@perforce.com>
2015-02-17 16:40:36 -0800

    Remove test changes that aren't required anymore.

--- perforce_swarm/spec/support/login_helpers.rb
+++ perforce_swarm/spec/support/login_helpers.rb
@@ -6,15 +6,4 @@ module LoginHelpers
     page.find(:css, 'header .profile-pic').click
     page.find(:css, 'header .logout').click
   end
-
-  # Internal: Login as the specified user
-  #
-  # user - User instance to login with
-  def login_with(user)
-    visit new_user_session_path
-    fill_in 'user_login', with: user.email
-    fill_in 'user_password', with: '12345678'
-    click_button 'Log in'
-    Thread.current[:current_user] = user
-  end
 end

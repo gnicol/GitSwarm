642de0f987a57b8f4d80fffc4adde880f6da69d2
Geoff Nicol <gnicol@perforce.com>
2015-02-20 14:27:49 -0800

    Example of programatic help adjustment

--- perforce_swarm/app/helpers/application_helper.rb
+++ perforce_swarm/app/helpers/application_helper.rb
@@ -15,4 +15,29 @@ module ApplicationHelper
   def promo_host
     'perforce.com'
   end
+
+  def help_preprocess(category, file)
+    # use our over-ride markdown if present, otherwise use their copy
+    if File.exists?(Rails.root.join('peforce_swarm', 'doc', category, file))
+      content = File.read(Rails.root.join('peforce_swarm', 'doc', category, file))
+    else
+      content = File.read(Rails.root.join('doc', category, file))
+    end
+
+    # do the work of the standard view
+    content.gsub!('$your_email', current_user.email)
+
+    # make some page specific tweaks
+    content.gsub!(/To see a more in-depth overview see the.*$/, '') if file == 'structure.md'
+
+    # they talk about GitLab EE only features, nuke those lines
+    content.gsub!(/^.*GitLab EE.*$/, '')
+
+    # rename to GitSwarm and ensure external links are theirs not ours
+    content.gsub!('GitLab', 'GitSwarm')
+    content.gsub!(/our website/i, "GitLab's website")
+
+    # return the munged string
+    content
+  end
 end

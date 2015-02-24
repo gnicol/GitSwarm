3a8ddd052aebddb46cde041758ae305e1c801d08
Geoff Nicol <gnicol@perforce.com>
2015-02-24 11:18:36 -0800

    Moved rebranding regex into a shared method/module.

--- perforce_swarm/lib/perforce_swarm/haml.rb
+++ perforce_swarm/lib/perforce_swarm/haml.rb
@@ -1,17 +1,11 @@
 module PerforceSwarm
   module HamlParserExtension
-    GITSWARM_REPLACE_REGEX = /GitLab(?!(\.com|\s+[Ff]lavored [Mm]arkdown|\$))/
     def parse_tag(line)
-      super translate(line)
+      super PerforceSwarm::Branding::rebrand(line)
     end
 
     def plain(text, escape_html = nil)
-      super translate(text), escape_html
-    end
-
-    def translate(text)
-      # ensure that instances of $GitLab$ are escaped from the translation, and get replaced with GitLab
-      text.gsub(GITSWARM_REPLACE_REGEX, 'GitSwarm').gsub(/\$GitLab\$/, 'GitLab')
+      super PerforceSwarm::Branding::rebrand(text), escape_html
     end
   end
 end
---
+++ perforce_swarm/lib/perforce_swarm/rebrand.rb
@@ -0,0 +1,11 @@
+module PerforceSwarm
+  module Branding
+    GITSWARM_REPLACE_REGEX = /GitLab(?!\.com|\s+[Ff]lavored [Mm]arkdown| [Ff]low| [Ww]orkflow| CI|\$)/
+
+    def self.rebrand(text)
+      # replace safe looking instances of GitLab with GitSwarm (skipping .com, Flavored Markdown, Literal instances)
+      # ensure that instances of $GitLab$ get replaced with literal word GitLab
+      text.gsub(GITSWARM_REPLACE_REGEX, 'GitSwarm').gsub('$GitLab$', 'GitLab')
+    end
+  end
+end

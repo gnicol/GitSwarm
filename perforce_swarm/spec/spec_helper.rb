8b39163e686fb17706e0f00ff2e66fa714e84e99
DJ Mountney <dmountney@perforce.com>
2015-03-17 12:11:30 -0700

    Increase the rack timeout
    Fixes an issue with the rspec tests failing on the first test, waiting for the app.

--- perforce_swarm/spec/spec_helper.rb
+++ perforce_swarm/spec/spec_helper.rb
@@ -1,5 +1,13 @@
 require File.expand_path('../../../spec/spec_helper', __FILE__)
 
-# Increase the timeout for all tests, not just JS tests
-Capybara.default_wait_time = 90
+# Increase the timeout for the swarm specs, as they may start with JS enabled
+# tests right from the start, and need to give rails some time to load
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, js_errors: false, timeout: 90)
+end
 
+RSpec.configure do
+  # Fixes Rack::Timeout::RequestTimeoutError by
+  # waiting longer than the default for our engine
+  Slowpoke.timeout = 90
+end

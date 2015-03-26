cd2a7a7dce0229aa017bcba7102e243107a7fae5
DJ Mountney <dmountney@perforce.com>
2015-03-26 16:12:05 -0700

    Throw errors in api/allowed when no service user
    On the /allowed route, we always throw errors if the repo is mirrored and the service user is missing

--- perforce_swarm/lib/api/internal.rb
+++ perforce_swarm/lib/api/internal.rb
@@ -1,9 +1,15 @@
 module API
   class Internal < Grape::API
     after_validation do
-      # Throw error if request includes the check_service_user flag, and the user is missing
-      if parse_boolean(params['check_service_user']) == true && !User.find_by(username: 'gitswarm')
-        render_api_error!('gitswarm user doesn\'t exist in GitSwarm', 400)
+      if options[:path].include?('/allowed')
+        project = Project.find_with_namespace(params[:project].chomp('.wiki'))
+
+        # Throw error if repo is mirrored, and the gitswarm user is missing
+        if project &&
+           PerforceSwarm::Mirror.mirror_url(project.repository.path_to_repo) &&
+           !User.find_by(username: 'gitswarm')
+          render_api_error!('gitswarm user doesn\'t exist in GitSwarm', 400)
+        end
       end
     end
   end

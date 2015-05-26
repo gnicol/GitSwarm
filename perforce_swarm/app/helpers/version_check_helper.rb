27c5269c0db7537d9b79508b3dd25dee6a38f47e
Elliot <ewiltshire@perforce.com>
2015-05-26 11:45:58 -0700

    Added last_version_ignored field to settings, so admins can remove the update nag.

--- perforce_swarm/app/helpers/version_check_helper.rb
+++ perforce_swarm/app/helpers/version_check_helper.rb
@@ -12,8 +12,14 @@ module VersionCheckHelper
       render('shared/version_check',
         status: version_check.status,
         more_info: version_check.more_info,
-        check_enabled: current_application_settings.version_check_enabled
+        check_enabled: current_application_settings.version_check_enabled,
+        latest: version_check.latest.to_s.gsub('.pre.', '-')
       )
     end
+  end
+
+  def version_check_enabled_link(enabled)
+    anchor = enabled ? 'Yes' : 'No'
+    link_to anchor, admin_application_setting_service_path(application_setting: {version_check_enable: enabled}), method: put, class: 'alert-link'
   end
 end

f23fe676a1f4f68f080c0482cfab3890c9cb93ea
Elliot <ewiltshire@perforce.com>
2015-05-29 11:56:05 -0700

    Review feedback.

--- perforce_swarm/app/helpers/version_check_helper.rb
+++ perforce_swarm/app/helpers/version_check_helper.rb
@@ -1,26 +1,14 @@
 require Rails.root.join('app', 'helpers', 'version_check_helper')
 
 module VersionCheckHelper
-  def version_check_enabled
-    current_application_settings.version_check_enabled.nil? || current_application_settings.version_check_enabled
-  end
+  def show_version_check?
+    # only admins who haven't dismissed the header get to see it
+    return false unless current_user.admin? && cookies[:dismiss_version_check].nil?
 
-  def version_check_ignored(latest)
-    current_application_settings.last_version_ignored &&
-    (latest == Gem::Version.new(current_application_settings.last_version_ignored))
-  end
+    # always try and show it if they haven't picked
+    return true if current_application_settings.version_check_enabled.nil?
 
-  def version_check_notification
-    version_check = VersionCheck.new
-    unless version_check.status == VersionCheck::VERSION_UNKNOWN || version_check_ignored(version_check.latest)
-      render('shared/version_check',
-        version_check: version_check
-      )
-    end
-  end
-
-  def version_check_enabled_link(enabled)
-    anchor = enabled ? 'Yes' : 'No'
-    link_to anchor, admin_application_setting_service_path(application_setting: {version_check_enable: enabled}), remote: true, method: :put, class: 'alert-link'
+    # if we're outdated and this particular update isn't ignored; show it!
+    VersionCheck::outdated? && VersionCheck::latest != current_application_settings.last_version_ignored
   end
 end

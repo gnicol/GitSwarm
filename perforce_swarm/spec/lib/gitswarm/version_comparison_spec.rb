09be8add4c0922b6af899a26bb88dbebd1d19904
Elliot <ewiltshire@perforce.com>
2015-06-04 15:19:51 -0700

    Updated to include new behaviour for absent build numbers.

--- perforce_swarm/spec/lib/gitswarm/version_comparison_spec.rb
+++ perforce_swarm/spec/lib/gitswarm/version_comparison_spec.rb
@@ -20,19 +20,19 @@ describe 'PerforceSwarm::VersionCheck Version Comparison', no_db: true do
   context '<' do
     it { expect(@v2015_1_beta).to be < @v2015_1_1 }
     it { expect(@v2015_1_beta).to be < @v2015_1 }
-    it { expect(@v2015_1).to be < @v2015_1_1 }
     it { expect(@v2015_2_1).to be < @v2016_1_1 }
     it { expect(@v2015_5_alpha).to be < @v2016_1_1 }
   end
 
   context '==' do
-    it { expect(@v2015_1).to be == VersionCheck.parse_version('2015.1-0') }
+    it { expect(@v2015_1).to be == @v2015_1_1 }
   end
 
   context 'valid version checks' do
     it { expect(VersionCheck.valid_version?('')).to be_falsey }
     it { expect(VersionCheck.valid_version?('lasldkasldkad')).to be_falsey }
     it { expect(VersionCheck.valid_version?('-0')).to be_falsey }
+    it { expect(VersionCheck.valid_version?('-1')).to be_falsey }
     it { expect(VersionCheck.valid_version?('2015.1')).to be_truthy }
     it { expect(VersionCheck.valid_version?('2015.1-beta')).to be_truthy }
   end

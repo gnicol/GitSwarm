7d7aa40f0382437377492d4458d58fa5a5fb935d
swellard <swellard@perforce.com>
2015-06-05 17:30:30 +0100

    Appearances helper test

---
+++ perforce_swarm/spec/helpers/appearances_helper_spec.rb
@@ -0,0 +1,10 @@
+require_relative '../spec_helper'
+
+describe AppearancesHelper do
+
+  describe 'brand_title' do
+    it 'returns GitSwarm always' do
+      expect(brand_title).to eq(AppearancesHelper::BRAND_TITLE_VALUE)
+    end
+  end
+end

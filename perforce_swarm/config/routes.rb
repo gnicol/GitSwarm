3c974dfbcc48dd6a501eafd377f1cff53cd03254
Elliot <ewiltshire@perforce.com>
2015-06-16 09:18:40 -0700

    Add Git Fusion repos to project absolute barebones.

--- perforce_swarm/config/routes.rb
+++ perforce_swarm/config/routes.rb
@@ -1,2 +1,9 @@
 Rails.application.routes.draw do
+  namespace :import do
+    resource :git_fusion, only: [:create, :new], controller: :git_fusion do
+      get :status
+      get :callback
+      get :jobs
+    end
+  end
 end

bb2a2ff22d5c94b021d0a53c7a7a4787915ad18f
Patrycja Tomiak <patrycjatomiak@gmail.com>
2015-07-02 17:16:12 +0100

    Perform settings update as an ajax call in version check

--- perforce_swarm/app/controllers/admin/application_settings_controller.rb
+++ perforce_swarm/app/controllers/admin/application_settings_controller.rb
@@ -5,6 +5,15 @@ module PerforceSwarm
     def application_setting_params
       super.merge(params.require(:application_setting).permit(:last_version_ignored))
     end
+
+    def update
+      # overwrite the update function to remove the redirect on success - instead just render page
+      # this enables an ajax call to settings update, and returns a flash notice
+      if @application_setting.update_attributes(application_setting_params)
+        flash.now[:notice] = 'Application settings saved successfully'
+      end
+      render :show
+    end
   end
 end
 

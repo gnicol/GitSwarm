874466b8b5def35ad9e173e94ebe50daa239b63f
Geoff Nicol <gnicol@perforce.com>
2015-07-09 16:08:18 -0700

    Just cleaning up formatting and adding comment details.
    I wanted to express that the current approach will only fetch
    once every 10 minutes despite appearing to do it every 5.

--- perforce_swarm/lib/perforce_swarm/mirror_fetch_worker.rb
+++ perforce_swarm/lib/perforce_swarm/mirror_fetch_worker.rb
@@ -8,11 +8,24 @@ module PerforceSwarm
     include Sidetiq::Schedulable
 
     # check every 5 minutes
+    # note this will only end up fetching every 10 minutes as we have a min-outdated of 5 minutes e.g.:
+    # - Starting at 10:00:00 am
+    # - fetch a (takes 6s) last_fetched = 10:00:06
+    # - fetch b (takes 2s) last_fetched = 10:00:08
+    #
+    # - Re-run at 10:05:00 am
+    # - fetch a skip (only 294 out of date)
+    # - fetch b skip (only 292 out of date)
+    #
+    # - Re-run at 10:10:00 am
+    # - fetches all the things as they are all sufficiently out of date
     recurrence { minutely(5) }
 
     def perform
-      mirror_script =
-          File.join(File.expand_path(Gitlab.config.gitlab_shell.path), 'perforce_swarm', 'bin', 'gitswarm-mirror')
+      # locate the gitlab-shell mirror script we'll be calling
+      shell_path    = File.expand_path(Gitlab.config.gitlab_shell.path)
+      mirror_script = File.join(shell_path, 'perforce_swarm', 'bin', 'gitswarm-mirror')
+
       # for each project, perform a mirror fetch
       Project.all.each do |project|
         puts "Running mirror fetch against #{project.path_with_namespace}"

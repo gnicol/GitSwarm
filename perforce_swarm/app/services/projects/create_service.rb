ebe17368d6e35e35dc682b9ea5519ee23cd61565
Geoff Nicol <gnicol@perforce.com>
2015-07-10 15:02:57 -0700

    Slight formatting tweaks.

--- perforce_swarm/app/services/projects/create_service.rb
+++ perforce_swarm/app/services/projects/create_service.rb
@@ -15,8 +15,8 @@ module PerforceSwarm
         # kick off and background initial import task
         log_info("Kicking off initial import for '#{@project.git_fusion_repo}'.")
         import_job = fork do
-          mirror_script =
-              File.join(File.expand_path(Gitlab.config.gitlab_shell.path), 'perforce_swarm', 'bin', 'gitswarm-mirror')
+          shell_path    = File.expand_path(Gitlab.config.gitlab_shell.path)
+          mirror_script = File.join(shell_path, 'perforce_swarm', 'bin', 'gitswarm-mirror')
           exec Shellwords.shelljoin([mirror_script, 'fetch', @project.path_with_namespace + '.git'])
         end
         Process.detach(import_job)

031a3ce85d938907e06bbca74667f5869aa0bc4d
Geoff Nicol <gnicol@perforce.com>
2015-07-13 11:30:01 -0700

    Removing limit: 255 from our db migration.
    The model handles it and having it in the schema seems outa line with gitlab.

--- perforce_swarm/db/migrate/20150702141633_add_git_fusion_repo_to_projects.rb
+++ perforce_swarm/db/migrate/20150702141633_add_git_fusion_repo_to_projects.rb
@@ -1,5 +1,5 @@
 class AddGitFusionRepoToProjects < ActiveRecord::Migration
   def change
-    add_column :projects, :git_fusion_repo, :string, limit: 255
+    add_column :projects, :git_fusion_repo, :string
   end
 end

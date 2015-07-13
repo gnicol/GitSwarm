018f531412a10ec616f3838276a492b7d5f01255
Geoff Nicol <gnicol@perforce.com>
2015-07-13 14:25:56 -0700

    Swapping Project add_import_job to using monkey-patching not prepend.
    The tests blow up when they try to stub the method out when we
    prepend our changes :(
    
    This works the same but doesn't kaboom

--- perforce_swarm/app/models/project.rb
+++ perforce_swarm/app/models/project.rb
@@ -1,26 +1,7 @@
 require Rails.root.join('app', 'models', 'project')
 
 module PerforceSwarm
-  module Project
-    def add_import_job
-      # no git fusion repo, so carry on with normal import behaviour
-      return super unless git_fusion_import?
-
-      # we have a git fusion import request- ensure project is marked as imported from git fusion
-      update_column(:import_type, 'git_fusion')
-
-      # create mirror remote
-      PerforceSwarm::Repo.new(repository.path_to_repo).mirror_url = git_fusion_repo
-
-      # kick off and background initial import task
-      import_job = fork do
-        gitlab_shell  = File.expand_path(Gitlab.config.gitlab_shell.path)
-        mirror_script = File.join(gitlab_shell, 'perforce_swarm', 'bin', 'gitswarm-mirror')
-        exec Shellwords.shelljoin([mirror_script, 'fetch', path_with_namespace + '.git'])
-      end
-      Process.detach(import_job)
-    end
-
+  module ProjectExtension
     def import_in_progress?
       return true if git_fusion_import? && import_status == 'started'
       super
@@ -34,5 +15,28 @@ end
 
 class Project < ActiveRecord::Base
   validates :git_fusion_repo, length: { maximum: 255 }, allow_blank: true
-  prepend PerforceSwarm::Project
+  prepend PerforceSwarm::ProjectExtension
+
+  # The rspec tests use 'allow_any_instance_of' on Project to stub this method out during testing.
+  # Unfortunately, if we 'prepend' our modifications that goes into an endless loop. So we monkey it.
+  # @todo If rspec ever fixed prepend handling; move this to ProjectExtension. Or fix rspec ourselves!
+  alias_method :add_import_job_super, :add_import_job
+  def add_import_job
+    # no git fusion repo, so carry on with normal import behaviour
+    return add_import_job_super unless git_fusion_import?
+
+    # we have a git fusion import request- ensure project is marked as imported from git fusion
+    update_column(:import_type, 'git_fusion')
+
+    # create mirror remote
+    PerforceSwarm::Repo.new(repository.path_to_repo).mirror_url = git_fusion_repo
+
+    # kick off and background initial import task
+    import_job = fork do
+      gitlab_shell  = File.expand_path(Gitlab.config.gitlab_shell.path)
+      mirror_script = File.join(gitlab_shell, 'perforce_swarm', 'bin', 'gitswarm-mirror')
+      exec Shellwords.shelljoin([mirror_script, 'fetch', path_with_namespace + '.git'])
+    end
+    Process.detach(import_job)
+  end
 end

f51993c904a2113dfcbe6ca57193f16d961e5132
DJ Mountney <dmountney@perforce.com>
2015-07-17 17:02:44 -0700

    Add a comment about why we are overwriting the other file services

--- perforce_swarm/app/services/files/delete_service.rb
+++ perforce_swarm/app/services/files/delete_service.rb
@@ -1,2 +1,4 @@
+# The file we are overwriting relative_requires its base_service, so we override it here
+# without adding any code for the sole purpose of getting our base service in as well
 require_relative 'base_service'
 require Rails.root.join('app', 'services', 'files', 'delete_service')

4d9d9c6b0b66d8488f4af54304d76bd4f8783477
Patrycja Tomiak <patrycjatomiak@gmail.com>
2015-08-21 15:42:58 +0100

    obsolete change

--- perforce_swarm/config/initializers/config.rb
+++ perforce_swarm/config/initializers/config.rb
@@ -8,6 +8,3 @@ module PerforceSwarm
     namespace Rails.env
   end
 end
-
-# gitswarm super user password for auto-provisioned p4d is in git-fusion's default configuration
-PerforceSwarm::Config['git_fusion']['default'] ||= Settingslogic.new({})

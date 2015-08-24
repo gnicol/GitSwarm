1b203f5b737d634bf5c87ce8d2816684ad6aa73c
Patrycja Tomiak <patrycjatomiak@gmail.com>
2015-08-24 15:45:22 +0100

    rubocop changes

--- perforce_swarm/app/controllers/profiles/passwords_controller.rb
+++ perforce_swarm/app/controllers/profiles/passwords_controller.rb
@@ -3,8 +3,9 @@ require Rails.root.join('app', 'controllers', 'profiles', 'passwords_controller'
 module PerforceSwarm
   module PasswordsControllerExtension
     def update
-      if @user.update_attributes(params[:password]) && @user.valid_password?(user_params[:current_password]) && @user.id == 1 &&
-          params[:user][:password] == params[:user][:password_confirmation]
+      if @user.update_attributes(params[:password]) &&
+         @user.valid_password?(user_params[:current_password]) && @user.id == 1 &&
+         params[:user][:password] == params[:user][:password_confirmation]
         p4 = PerforceSwarm::P4Connection.new
         message = p4.change_root_password(params[:user][:password])
         render 'edit' && return if message

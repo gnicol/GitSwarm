07cdbbe11e5874794bffdfd077ea5510515302fe
DJ Mountney <dmountney@perforce.com>
2015-08-25 14:54:27 -0700

    Added previous work on convention UI to lastest UI multiple GF work
    This is a work in progress, and includes some work that is pending in merge requests against prep fore creating new repos.

--- perforce_swarm/app/controllers/projects_controller.rb
+++ perforce_swarm/app/controllers/projects_controller.rb
@@ -3,6 +3,8 @@ require Rails.root.join('app', 'controllers', 'projects_controller')
 module PerforceSwarm
   module ProjectsControllerExtension
     def project_params
+      params[:git_fusion_auto_create] = params[:git_fusion_auto_create] == 'true'
+
       # if we were given git fusion parameters, incorporate those now
       if params[:git_fusion_entry] &&
          !params[:git_fusion_entry].blank? &&
@@ -11,7 +13,7 @@ module PerforceSwarm
         params[:git_fusion_repo] = "mirror://#{params[:git_fusion_entry]}/#{params[:git_fusion_repo_name]}"
       end
 
-      super.merge(params.permit(:git_fusion_repo))
+      super.merge(params.permit(:git_fusion_repo, :git_fusion_auto_create))
     end
   end
 end
--- perforce_swarm/app/models/project.rb
+++ perforce_swarm/app/models/project.rb
@@ -8,14 +8,32 @@ module PerforceSwarm
     end
 
     def git_fusion_import?
-      git_fusion_repo.present?
+      git_fusion_repo.present? && !git_fusion_auto_create
+    end
+
+    def create_repository
+      if !git_fusion_entry.blank? && git_fusion_auto_create
+        # creator = PerforceSwarm::GitFusion::RepoCreator.new(git_fusion_entry, namespace: self.namespace.name, project
+        # puts creator.generate_depot_path
+        git_fusion_repo = "mirror://#{git_fusion_entry}/#{path}"
+      end
+
+      super
     end
   end
 end
 
 class Project < ActiveRecord::Base
   validates :git_fusion_repo, length: { maximum: 255 }, allow_blank: true
+  validates :git_fusion_repo, presence: true, unless: "git_fusion_entry.blank? || git_fusion_auto_create"
   prepend PerforceSwarm::ProjectExtension
+
+  attr_accessor :git_fusion_auto_create
+  attr_accessor :git_fusion_entry
+
+  # run our code after the GL validations are done
+  # before_save :auto_create_git_fusion, if: :git_fusion_auto_create
+
 
   # The rspec tests use 'allow_any_instance_of' on Project to stub this method out during testing.
   # Unfortunately, if we 'prepend' our modifications that goes into an endless loop. So we monkey it.

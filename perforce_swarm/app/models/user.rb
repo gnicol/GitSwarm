3b0db32bc924b6c8a547ed9b794265aff771590a
Patrycja Tomiak <patrycjatomiak@gmail.com>
2015-08-26 09:35:34 +0100

    Move logic to user model for password change

--- perforce_swarm/app/models/user.rb
+++ perforce_swarm/app/models/user.rb
@@ -3,26 +3,38 @@ require Rails.root.join('app', 'models', 'user')
 module PerforceSwarm
   module UserExtension
     def save!
-      if username == 'root'
-        p4 = PerforceSwarm::P4Connection.new
-        message = p4.change_root_password(password)
-        return message if message
-      end
+      message = self.sync_p4d_password(password)
+      return message unless message.is_a? Array
       super
     end
 
     def validate_and_change_in_perforce
-      if username == 'root'
-        p4 = PerforceSwarm::P4Connection.new
-        message = p4.change_root_password(password)
-        errors.add(:base, message) unless message.is_a? Array
-        return false unless message.is_a? Array
+      message = self.sync_p4d_password(password)
+      errors.add(:base, message) unless message.is_a? Array
+      return false unless message.is_a? Array
+      super
+    end
+
+    def sync_p4d_password(password)
+      # presently we only handle the 'root' user and only for auto-provisioned servers
+      return unless self.username == 'root'
+      default_config = PerforceSwarm::GitlabConfig.new.git_fusion.entry('default')
+      return unless default_config['auto_provision'] == true
+
+      connection = PerforceSwarm::P4::Connection.new(default_config)
+      connection.login
+      connection.input(password)
+      begin
+        connection.run('passwd', 'root')
+      rescue P4Exception => ex
+        message = ex.message.match(/\[Error\]: (?<message>.*)$/)
+        message['message']
       end
     end
   end
 end
 
 class User < ActiveRecord::Base
-  before_validation :validate_and_change_in_perforce
+  before_save :validate_and_change_in_perforce
   prepend PerforceSwarm::UserExtension
 end

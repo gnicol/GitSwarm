27d397511d0634bda3791dee2e49b7ce6ae302a6
Patrycja Tomiak <patrycjatomiak@gmail.com>
2015-08-27 10:09:15 +0100

    Further changes to validate

--- perforce_swarm/app/models/user.rb
+++ perforce_swarm/app/models/user.rb
@@ -8,17 +8,22 @@ module PerforceSwarm
       super
     end
 
-    def validate_and_change_in_perforce
-      message = sync_p4d_password(password)
-      errors.add(:base, message) unless message.is_a? Array
-      return false unless message.is_a? Array
+    def validate_and_change_in_p4d
+      # presently we only handle the 'root' user and only for auto-provisioned servers
+      # run only if were changing password
+      return unless @changed_attributes.key?('encrypted_password') && username == 'root'
+      sync_p4d_password(password)
+    rescue P4Exception => ex
+      errors.add(:base, ex.message)
+      return false
+    rescue RuntimeError
+      # rescue RunTime error - which will be raised if git fusion config does not have a default
+      return true
     end
 
     def sync_p4d_password(password)
-      # presently we only handle the 'root' user and only for auto-provisioned servers
-      return unless username == 'root'
       default_config = PerforceSwarm::GitlabConfig.new.git_fusion.entry('default')
-      return unless default_config['auto_provision'] == true
+      return unless default_config['auto_provision']
 
       connection = PerforceSwarm::P4::Connection.new(default_config)
       connection.login
@@ -26,14 +31,14 @@ module PerforceSwarm
       begin
         connection.run('passwd', 'root')
       rescue P4Exception => ex
-        message = ex.message.match(/\[Error\]: (?<message>.*)$/)
-        message['message']
+        message = Regexp.last_match(:error) if ex.message.match(/\[Error\]: (?<error>.*)$/)
+        raise ex, message
       end
     end
   end
 end
 
 class User < ActiveRecord::Base
-  before_save :validate_and_change_in_perforce
+  before_save :validate_and_change_in_p4d
   prepend PerforceSwarm::UserExtension
 end

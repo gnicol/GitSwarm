f2cd06144e018a2fe1dac0622a3aeee273d94a19
mphillips <mphillips@perforce.com>
2015-10-12 11:46:34 +0100

    enhance tests to check re-mirrored project contains existing content and existing GF repo shows up in list of available repos.

--- perforce_swarm/validation_tests/spec/mirroring_spec.rb
+++ perforce_swarm/validation_tests/spec/mirroring_spec.rb
@@ -10,15 +10,19 @@ describe 'New Mirrored Project', browser: true do
   let(:uemail) { 'p4cloudtest+'+user+'@gmail.com' }
   let(:email) { 'root@mp-gs-ubuntu-12-153' }
   let(:project) { 'project-'+unique_string }
+  let(:expected_gf_repo_name) { 'gitswarm-'+user+'-'+project}
   let(:git_dir) { Dir.mktmpdir }
   let(:p4_dir) {  Dir.mktmpdir }
+  let(:another_project) { 'another_project-'+unique_string }
+  let(:another_git_dir) { Dir.mktmpdir }
 
   before do
     create_user
     create_new_project
-    clone_project
+    clone_project(project, git_dir)
 
     LOG.debug 'p4 dir = ' + p4_dir
+    LOG.debug 'user is ' + user + ' : ' + password
     p4_depot_path = CONFIG.get('p4_gitswarm_depot_root') + user + '/' + project + '/master/...'
     @p4 = P4Helper.new(CONFIG.get('p4_port'), CONFIG.get('p4_user'), CONFIG.get('p4_password'), p4_dir, p4_depot_path)
   end
@@ -30,10 +34,10 @@ describe 'New Mirrored Project', browser: true do
       LOG.debug 'Creating file in git : ' + @git_filename
       @git.add_commit_push
       sleep(5) # some time to let the file get into perforce
-      @p4.connect_and_sync
     end
 
     it 'gets pushed into Perforce' do
+      @p4.connect_and_sync
       p4_file_from_git = p4_dir + '/' + @git_filename
       expect(File.exist?(p4_file_from_git)).to be true
     end
@@ -56,6 +60,48 @@ describe 'New Mirrored Project', browser: true do
     end
   end
 
+  context 'when a repo was created in GitFusion' do
+    it 'is visible in the list of available repos' do
+      cp = LoginPage.new(@driver, CONFIG.get('gitswarm_url')).login(user, password).goto_create_project_page
+      available_repos = cp.repo_names
+      LOG.log('expecting '+ expected_gf_repo_name)
+      available_repos.each { |x| LOG.log(x)}
+      expect(available_repos.include?(expected_gf_repo_name)).to be true
+    end
+  end
+
+  context 'when an existing repo with content is mirrored in a new project' do
+    before do
+      @git_filename = 'git-file-'+unique_string
+      create_file(git_dir, @git_filename)
+      LOG.debug 'Creating file in git : ' + @git_filename
+      @git.add_commit_push
+
+      @p4.connect_and_sync
+      @p4_filename = 'p4-file-'+unique_string
+      add_path = create_file(p4_dir, @p4_filename)
+      LOG.debug 'Creating file in p4 : ' + add_path
+      @p4.add(add_path)
+      @p4.submit
+
+      cp = LoginPage.new(@driver, CONFIG.get('gitswarm_url')).login(user, password).goto_create_project_page
+      cp.project_name(another_project)
+      cp.select_mirrored_specific
+      cp.select_repo(expected_gf_repo_name)
+      cp.create_project_and_wait_for_clone
+    end
+
+    it 'contains files from that repo' do
+
+      clone_project(another_project, another_git_dir)
+      existing_git_file_from_p4 = another_git_dir + '/' + @p4_filename
+      existing_p4_file_from_git = another_git_dir + '/' + @git_filename
+      expect(File.exist?(existing_git_file_from_p4)).to be true
+      expect(File.exist?(existing_p4_file_from_git)).to be true
+
+    end
+  end
+
   def create_user
     GitSwarmAPIHelper.new(CONFIG.get('gitswarm_url'),
                           CONFIG.get('gitswarm_username'),
@@ -68,15 +114,16 @@ describe 'New Mirrored Project', browser: true do
     cp.project_name(project)
     cp.select_mirrored_auto
     cp.create_project_and_wait_for_clone
+    cp.logout
   end
 
-  def clone_project
+  def clone_project(project_name, dir)
     user_helper = GitSwarmAPIHelper.new(CONFIG.get('gitswarm_url'), user, password)
-    project_info = user_helper.get_project_info(project)
+    project_info = user_helper.get_project_info(project_name)
     LOG.debug project_info
-    LOG.debug 'git dir = ' + git_dir
+    LOG.debug 'git dir = ' + dir
 
-    @git = GitHelper.http_helper(git_dir, project_info[GitSwarmAPIHelper::HTTP_URL], user, password, email)
+    @git = GitHelper.http_helper(dir, project_info[GitSwarmAPIHelper::HTTP_URL], user, password, email)
     @git.clone
   end
 end

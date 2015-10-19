cecb50a3efa5509ac58a7aca8bbf39a7c550d9b3
DJ Mountney <dmountney@perforce.com>
2015-10-19 14:19:36 -0700

    No longer use the rugged commit object for mirroring, it is used for too many non-mirrored things now. We will the mirroring later in repository.commit_with_hooks which is where we will do all the push preflight
    Also updated the merge request service to fetch from mirror first when attempting to do the merge request.

---
+++ perforce_swarm/app/services/merge_requests/merge_service.rb
@@ -0,0 +1,15 @@
+require Rails.root.join('app', 'services', 'merge_requests', 'merge_service')
+
+module PerforceSwarm
+  module MergeServiceExtensions
+    def execute(merge_request, commit_message)
+      # Force a fetch from mirror before merging
+      merge_request.check_if_can_be_merged
+      super
+    end
+  end
+end
+
+class MergeRequests::MergeService
+  prepend PerforceSwarm::CreateServiceExtensions
+end

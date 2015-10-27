2555e207ae9376d8bcf4054c0d12c45142d193be
mphillips <mphillips@perforce.com>
2015-10-27 17:12:01 +0000

    proper fix to have lov level settable in config.yml

--- perforce_swarm/validation_tests/lib/log.rb
+++ perforce_swarm/validation_tests/lib/log.rb
@@ -28,11 +28,18 @@ class LOG
     File.delete(logfile) if File.exist?(logfile)
     @internal_log ||= Logger.new MultiIO.new(STDOUT, File.open(logfile, 'a'))
     @internal_log.info('Logging to ' + logfile)
-    @internal_log.level = Logger::DEBUG # set default log level to debug for test runs
+    @internal_log.level = Logger::INFO # set default log level to info
   end
 
   def self.level(level)
-    @internal_log.level = level
+    if level=='INFO'
+      @internal_log.level = Logger::INFO
+    elsif level=='DEBUG'
+      @internal_log.level = Logger::DEBUG
+    else
+      fail('Unknown log level supplied : ' + level)
+    end
+    @internal_log.info("Log level set to #{level}")
   end
 
   def self.info(message)

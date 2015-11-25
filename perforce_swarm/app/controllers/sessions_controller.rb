71a93c8d18b05571863f834eabbd33502f2c9bc6
David Mountney <dmountney@perforce.com>
2015-11-25 10:34:40 -0800

    Update the call to the git_fusion_repo cache method

--- perforce_swarm/app/controllers/sessions_controller.rb
+++ perforce_swarm/app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ module PerforceSwarm
   module SessionsControllerExtension
     def create
       super
-      current_user.clear_git_fusion_repo_access_cache if current_user
+      current_user.clear_git_fusion_repo_cache if current_user
     end
   end
 end

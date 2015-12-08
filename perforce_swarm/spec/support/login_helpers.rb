34f999a4b7b23194e70459c8af8741c6d5c0c0e3
Elliot <ewiltshire@perforce.com>
2015-12-08 10:01:32 -0800

    Swapping the mocked URL to a regex so we can include EE in one shot.

--- perforce_swarm/spec/support/login_helpers.rb
+++ perforce_swarm/spec/support/login_helpers.rb
@@ -9,5 +9,5 @@ module LoginHelpers
 end
 
 # stub requests to updates.perforce.com for check for updates
-WebMock.stub_request(:get, 'https://updates.perforce.com/static/GitSwarm/GitSwarm.json')
+WebMock.stub_request(:get, %r{https://updates\.perforce\.com/static/GitSwarm/GitSwarm(\-ee)?\.json})
   .to_return(status: 200, body: '', headers: {})

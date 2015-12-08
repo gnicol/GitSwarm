7e73721459c41a7ee284fdfa4c1c0ebc553d77f5
Elliot <ewiltshire@perforce.com>
2015-12-08 09:28:37 -0800

    Removing headers to make the mock work for more requests.

--- perforce_swarm/spec/support/login_helpers.rb
+++ perforce_swarm/spec/support/login_helpers.rb
@@ -10,9 +10,4 @@ end
 
 # stub requests to updates.perforce.com for check for updates
 WebMock.stub_request(:get, 'https://updates.perforce.com/static/GitSwarm/GitSwarm.json')
-  .with(headers: { 'Accept'=>'*/*',
-                   'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-                   'User-Agent'=>'Ruby'
-                 }
-        )
   .to_return(status: 200, body: '', headers: {})

8a20c759eaf4a3ecd0e307bd6a7d4e32ee291b6b
Elliot <ewiltshire@perforce.com>
2015-12-07 16:49:00 -0800

    Adding WebMock for updates.perforce.com.

--- perforce_swarm/spec/support/login_helpers.rb
+++ perforce_swarm/spec/support/login_helpers.rb
@@ -7,3 +7,12 @@ module LoginHelpers
     page.find(:css, 'header .logout').click
   end
 end
+
+# stub requests to updates.perforce.com for check for updates
+WebMock.stub_request(:get, 'https://updates.perforce.com/static/GitSwarm/GitSwarm.json')
+  .with(headers: { 'Accept'=>'*/*',
+                   'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+                   'User-Agent'=>'Ruby'
+                 }
+        )
+  .to_return(status: 200, body: '', headers: {})

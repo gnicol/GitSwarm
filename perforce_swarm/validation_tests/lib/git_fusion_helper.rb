42d8640efddc0a2930f1ef55da20fadc8d881e4d
mphillips <mphillips@perforce.com>
2015-12-10 12:08:25 +0000

    Style changes from review comments

--- perforce_swarm/validation_tests/lib/git_fusion_helper.rb
+++ perforce_swarm/validation_tests/lib/git_fusion_helper.rb
@@ -2,29 +2,26 @@ require_relative 'log'
 
 class GitFusionHelper
   def initialize(p4port, user, password)
-    @user = user
+    @user     = user
     @password = password
-    @p4port = p4port
+    @p4port   = p4port
   end
 
   # depot_path should not include trailing /...
   def make_new_gf_repo(gf_repo_name, depot_path)
     LOG.log("Making new gf repo #{gf_repo_name} at #{@p4port}")
-    local_workspace =  Dir.mktmpdir
-    path = local_workspace+'/p4gf_config'
-    begin
-      p4 = P4Helper.new(@p4port, @user, @password, local_workspace, "//.git-fusion/repos/#{gf_repo_name}/...")
-      p4.connect_and_sync
-      fail("Config file already exists for GF repo #{gf_repo_name}") if File.exist?(path)
-      new_file = File.open(path, 'w+')
-      new_file.write gf_config_contents(depot_path)
-      new_file.close
-
-      p4.add(path)
-      p4.submit
-    ensure
-      p4.disconnect if p4
-      FileUtils.rm_r(local_workspace)
+    Dir.mktmpdir('GFHelper-', tmp_client_dir) do | local_workspace |
+      path = local_workspace + '/p4gf_config'
+      begin
+        p4 = P4Helper.new(@p4port, @user, @password, local_workspace, "//.git-fusion/repos/#{gf_repo_name}/...")
+        p4.connect_and_sync
+        fail("Config file already exists for GF repo #{gf_repo_name}") if File.exist?(path)
+        File.write(path, gf_config_contents(depot_path))
+        p4.add(path)
+        p4.submit
+      ensure
+        p4.disconnect if p4
+      end
     end
   end
 

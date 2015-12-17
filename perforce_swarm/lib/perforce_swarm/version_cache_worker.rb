3575c6f5b985f4a5789733387088979df5320147
DJ Mountney <dmountney@perforce.com>
2015-12-17 11:03:15 -0800

    Move from sidetiq to sidekiq-cron to match the GitLab community

--- perforce_swarm/lib/perforce_swarm/version_cache_worker.rb
+++ perforce_swarm/lib/perforce_swarm/version_cache_worker.rb
@@ -1,16 +1,11 @@
 require 'rubygems'
 require 'sidekiq'
-require 'sidetiq'
 require Rails.root.join('lib', 'gitlab', 'current_settings')
 
 module PerforceSwarm
   class VersionCacheWorker
     include Sidekiq::Worker
-    include Sidetiq::Schedulable
     include Gitlab::CurrentSettings
-
-    # randomize when the worker runs to be on a random minute of every day
-    recurrence { daily.hour_of_day(rand(24)).minute_of_hour(rand(60)) }
 
     def perform
       # we only run the automated check if the admin has explicitly enabled it

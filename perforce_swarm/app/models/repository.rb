ca68676af22546182b77ec408cbb747494c8be4a
DJ Mountney <dmountney@perforce.com>
2016-01-07 15:49:55 -0800

    Add locking to add and remove branch actions on the repo

--- perforce_swarm/app/models/repository.rb
+++ perforce_swarm/app/models/repository.rb
@@ -12,6 +12,28 @@ module PerforceSwarm
       super
     end
 
+    def add_branch(user, branch_name, target)
+      begin
+        socket_server = PerforceSwarm::MirrorLockSocketServer.new(path_to_repo)
+        socket_server.start
+        status = super
+      ensure
+        socket_server.stop if socket_server
+      end
+      status
+    end
+
+    def rm_branch(user, branch_name)
+      begin
+        socket_server = PerforceSwarm::MirrorLockSocketServer.new(path_to_repo)
+        socket_server.start
+        status = super
+      ensure
+        socket_server.stop if socket_server
+      end
+      status
+    end
+
     def commit_with_hooks(current_user, branch)
       begin
         socket_server = PerforceSwarm::MirrorLockSocketServer.new(path_to_repo)

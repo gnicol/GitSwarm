f45f24d1c6f73aad32bb6831df43ee5d91ed80c7
Elliot Wiltshire <ewiltshire@perforce.com>
2016-04-26 12:12:22 -0700

    Incorporate milestones changes so tests pass.

--- config/routes.rb
+++ config/routes.rb
@@ -351,8 +351,6 @@ Rails.application.routes.draw do
     get :issues
     get :merge_requests
     get :activity
-    get :labels
-    get :milestones
 
     scope module: :dashboard do
       resources :milestones, only: [:index, :show]

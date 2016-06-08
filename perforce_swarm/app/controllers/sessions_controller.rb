bb138b55370a9785fc0d59eff0b633e7537fa265
Elliot Wiltshire <ewiltshire@perforce.com>
2016-06-07 21:29:35 -0700

    Make the initial code work with either GitLab or GitSwarm settings.

--- perforce_swarm/app/controllers/sessions_controller.rb
+++ perforce_swarm/app/controllers/sessions_controller.rb
@@ -8,16 +8,17 @@ module PerforceSwarm
     end
 
     # handle an "initial setup" state, where we have:
-    # 1) two users
-    # 2) one admin user
-    # 3) one non-admin user called "gitswarm"
-    # 3) the admin user requires a password change
+    # 1) one or two users
+    # 2) one admin user that requires a password change
+    # 3) if a non-admin user is given, it has the username 'gitswarm'
     def check_initial_setup
-      return unless User.count == 2
+      return unless User.count <= 2
 
-      # look for a regular user called 'gitswarm'
-      user = (User.all - User.admins).last
-      return unless user && user.username == 'gitswarm'
+      # look for a regular user called 'gitswarm' if we have 2 users
+      if User.count == 2
+        user = (User.all - User.admins).last
+        return unless user && user.username == 'gitswarm'
+      end
 
       # look for an admin user that requires a password
       admin = User.admins.last

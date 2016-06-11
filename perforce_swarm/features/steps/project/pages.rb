ab24d1071831c8663f5e70a39e8760cb883e7c63
Elliot Wiltshire <ewiltshire@perforce.com>
2016-06-11 08:45:55 -0700

    Fixing EE-only test.

--- perforce_swarm/features/steps/project/pages.rb
+++ perforce_swarm/features/steps/project/pages.rb
@@ -1,4 +1,5 @@
-require Rails.root.join('features', 'steps', 'project', 'pages')
+# file only present in EE
+require Rails.root.join('features', 'steps', 'project', 'pages') if PerforceSwarm.ee?
 
 class Spinach::Features::ProjectPages < Spinach::FeatureSteps
   # EE-only step
